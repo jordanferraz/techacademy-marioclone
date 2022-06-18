40fb0971f763087f5d0e7ca85c6303e1c865b04d
Jordan Ferraz <jordanferraz@gmail.com>
2022-06-18 13:31:01 -0300

    add mobile responsivity v1.0.0

--- js/marioscript.js
+++ js/marioscript.js
@@ -1,6 +1,10 @@
 const mario = document.querySelector('.mario');
 const pipe = document.querySelector('.pipe');
 
+let width = screen.width;
+
+if(width > 768){
+
 const jump = () => {
     mario.classList.add('jump');
 
@@ -10,6 +14,7 @@ const jump = () => {
 };
 
 document.addEventListener('keydown', jump);
+document.addEventListener('touchstart', jump);
 
 const loopGame = setInterval(() => {
 
@@ -32,3 +37,43 @@ const loopGame = setInterval(() => {
     }
 
 },10);
+
+}else{
+
+    const jump = () => {
+        mario.classList.add('jump');
+    
+        setTimeout(() =>{
+            mario.classList.remove('jump');
+        }, 1000);
+    };
+    
+    document.addEventListener('keydown', jump);
+    document.addEventListener('touchstart', jump);
+    
+    const loopGame = setInterval(() => {
+    
+        const pipePosition = pipe.offsetLeft;
+        const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
+        console.log(marioPosition);
+    
+        if(pipePosition <= 110 && pipePosition > 40 && marioPosition < 60){
+            pipe.style.animation = 'none';
+            pipe.style.left = `${pipePosition}px`;
+    
+            mario.style.animation = 'none';
+            mario.style.bottom = `${marioPosition}px`;           
+    
+            mario.src = './images/mario-game-over.png';
+            mario.style.width = '50px';
+            mario.style.bottom = '80px';
+            mario.style.left = '26px';
+    
+            clearInterval(loopGame);
+            
+            
+        }
+    
+    },10);
+
+}
